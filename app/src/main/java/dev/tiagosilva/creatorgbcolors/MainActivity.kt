887cfd1203fdcef69f0f6897016e9c3af647f440
Tiago Silva <tiagoluizrs@gmail.com>
2023-02-15 21:45:08 -0300

    Initial commit

---
+++ app/src/main/java/dev/tiagosilva/creatorgbcolors/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.tiagosilva.creatorgbcolors
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
